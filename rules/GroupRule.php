90bef7dbb17cc2d4c58df0ded04f951e7fa018b2
Tutik Alexsandr <sanchezzzhak@ya.ru>
2023-03-24 14:43:23 +0300

    Update GroupRule.php

--- rules/GroupRule.php
+++ rules/GroupRule.php
@@ -23,30 +23,12 @@ class GroupRule extends Rule implements IGroupRole
     public function execute($user, $item, $params)
     {
         if (!Yii::$app->user->isGuest) {
-
             /** @var \app\models\User $user */
             $user = Yii::$app->user->identity;
             $role = $user->role;
-
-            if ($item->name === self::ROLE_SUPER_ADMIN) {
-                return $role === $item->name;
-
-            } elseif ($item->name === self::ROLE_ADMIN) {
-                return $role === $item->name || $role === self::ROLE_SUPER_ADMIN;
-
-            }else if($item->name === self::ROLE_MANAGER){
-                return $role === $item->name
-                    || $role === self::ROLE_SUPER_ADMIN
-                    || $role === self::ROLE_USER
-                    || $role === self::ROLE_ADMIN;
-
-            } elseif ($item->name === self::ROLE_USER) {
-                return $role === $item->name
-                    || $role === self::ROLE_SUPER_ADMIN
-                    || $role === self::ROLE_ADMIN
-                    || $role === self::ROLE_MANAGER;
-            }
+            return ($role === $item->name);
         }
+        
         return false;
     }
 }
